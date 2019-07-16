392bd65a6ca8a7794993277584cf20736ed82b75
nsamat <nsamat@ebay.com>
2019-07-16 17:52:59 +0800

    add comments

--- proxyShell.sh
+++ proxyShell.sh
@@ -1,11 +1,9 @@
 #!/bin/bash
 
 # tunnel_port is specific port number of proxy
-# target_user is target host login user name
-# target_host is the target host that to build proxy
+# proxy_user is proxy host login user name
+# proxy_host is the proxy host that to build proxy
 # set tunnel_port variable or use specific port number to instead $tunnel_port in below command
-# set target_user and target_host variables or use specific user name and host name to instead them in below command
+# set proxy_user and proxy_host variables or use specific user name and host name to instead them in below command
  
-ssh -L $tunnel_port:127.0.0.1:$tunnel_port $target_user@$target_host
-
-
+ssh -L $tunnel_port:127.0.0.1:$tunnel_port $proxy_user@$proxy_host
